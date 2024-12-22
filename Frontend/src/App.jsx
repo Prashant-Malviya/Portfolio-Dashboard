59746446f0e10fa48fdff6768edce64f9cf4b450
Prashant Malviya <prashantmalviya272002@gmail.com>
2024-12-23 04:25:47 +0530

    code updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import Content from "./pages/Content";
-import SmallNavbar from "./components/smallNavbar";
 import { BrowserRouter as Router } from "react-router-dom";
+import SmallNavbar from "./components/smallNavbar";
 
 function App() {
   return (
-      <Router>
-    <div className="bg-gray-100 min-h-screen">
-      <Header />
+    <Router>
+      <div className="bg-gray-100 min-h-screen">
+        <Header />
         <Content />
-      <Sidebar />
-      <SmallNavbar />
-    </div>
-      </Router>
+        <Sidebar />
+        <SmallNavbar />
+      </div>
+    </Router>
   );
 }
 
